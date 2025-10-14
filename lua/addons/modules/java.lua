593530b205a4a6b786d1a20d0325d7041974d507
Esad Mustafoski <3027092@stud.hs-mannheim.de>
2025-10-14 14:57:26 +0200

    Removed Java Keybinds (Didn't work)

--- lua/addons/modules/java.lua
+++ lua/addons/modules/java.lua
@@ -8,15 +8,6 @@ return {
         init = function()
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = "java",
-                callback = function()
-                    local wk = require("which-key")
-                    wk.add({
-                        { "<leader>y",  group = "Java",                      buffer = 0 },
-                        { "<leader>yr", "<cmd>JavaRunnerRunMain<CR>",        desc = "Run Main",    buffer = 0 },
-                        { "<leader>yt", "<cmd>JavaTestRunCurrentClass<CR>",  desc = "Test Class",  buffer = 0 },
-                        { "<leader>ym", "<cmd>JavaTestRunCurrentMethod<CR>", desc = "Test Method", buffer = 0 },
-                    })
-                end,
             })
         end,
     },
