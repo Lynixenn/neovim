01e8e3e852a382ab2662ef1f6237136bf57f018b
Esad Mustafoski <3027092@stud.hs-mannheim.de>
2025-10-22 08:26:52 +0200

    removed init

--- lua/addons/modules/java.lua
+++ lua/addons/modules/java.lua
@@ -4,14 +4,7 @@ return {
         {
             "nvim-java/nvim-java",
             ft = { "java" },
-            config = function()
-                require('java').setup()
-            end,
-            init = function()
-                vim.api.nvim_create_autocmd("FileType", {
-                    pattern = "java",
-                })
-            end,
         },
     }
 }
+
