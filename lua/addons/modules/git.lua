23ce37a6fe4d34e318aeb2e9f5482576ffc3021f
Esad Mustafoski <3027092@stud.hs-mannheim.de>
2025-10-22 02:20:18 +0200

    Changed to VSCode theme

--- lua/addons/modules/git.lua
+++ lua/addons/modules/git.lua
@@ -14,11 +14,6 @@ return {
                 "DiffviewFocusFiles",
                 "DiffviewFileHistory"
             },
-            keys = {
-                { "<leader>dv", "<cmd>DiffviewOpen<cr>",          desc = "Open Diffview" },
-                { "<leader>dc", "<cmd>DiffviewClose<cr>",         desc = "Close Diffview" },
-                { "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
-            },
             opts = {
                 view = {
                     merge_tool = {
@@ -63,3 +58,4 @@ return {
         },
     }
 }
+
